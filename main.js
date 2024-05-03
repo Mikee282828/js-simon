35b9cca0748270d34168b8ab2723c42d14ec8a80
MikeDesktop <micheledong2803@gmail.com>
2024-05-03 19:07:56 +0200

    m3 - mostra prompt che chiede 5 numeri

--- main.js
+++ main.js
@@ -1,21 +1,32 @@
 let areaGioco = document.getElementById("areaGioco");
+let numeriGiocatore = [];
 //genera numeri casuali con la funzione randomNum()
 let numeriCasuali = randomNum();
 
 //stampa numeri casuali 
-for(i = 0; i<5; i++){
-    areaGioco.innerHTML+= `<h2>${numeriCasuali[i]}</h2>`
+for (i = 0; i < 5; i++) {
+    areaGioco.innerHTML += `<h2>${numeriCasuali[i]}</h2>`
 }
 
-//timer di 3 secondi e nascondi i numeri stampati in pagina
-setTimeout(function(){
-    areaGioco.innerHTML="";
-},3000);
+//timer di 3 secondi
+setTimeout(function () {
+    //nascondi i numeri stampati 
+    areaGioco.innerHTML = "";
+    //mostra un prompt che chiede 5 numeri 
+    setTimeout(function () {
+        for (i = 0; i < 5; i++) {
+            numeriGiocatore[i] = parseInt(prompt("Inserisci i numeri che hai visto prima"));
+        }
+    },10);
 
-function randomNum(){
+}, 3000);
+
+
+
+function randomNum() {
     let Lista = [];
-    for(let i = 0; i<5; i++){
-        Lista[i] = Math.round(Math.random()*100);
+    for (let i = 0; i < 5; i++) {
+        Lista[i] = Math.round(Math.random() * 100);
     }
     return Lista;
 }
