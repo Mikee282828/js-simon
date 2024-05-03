212e8b3420e56a4d1ec5f8fb6fc6a9a61dcc77b8
MikeDesktop <micheledong2803@gmail.com>
2024-05-03 19:02:55 +0200

    m2 - nascondo i numeri dopo 3 secondi

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 let areaGioco = document.getElementById("areaGioco");
+//genera numeri casuali con la funzione randomNum()
 let numeriCasuali = randomNum();
 
+//stampa numeri casuali 
 for(i = 0; i<5; i++){
-    areaGioco.innerHTML+= `<span>${numeriCasuali[i]}</span><br>`
+    areaGioco.innerHTML+= `<h2>${numeriCasuali[i]}</h2>`
 }
 
+//timer di 3 secondi e nascondi i numeri stampati in pagina
+setTimeout(function(){
+    areaGioco.innerHTML="";
+},3000);
 
 function randomNum(){
     let Lista = [];
